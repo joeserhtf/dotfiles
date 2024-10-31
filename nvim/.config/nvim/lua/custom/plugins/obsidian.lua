573292797abd99267d7e3b69af7caf240c8c2a60
JoeserHtf <joeserhtf@hotmail.com>
2024-10-31 00:14:31 -0300

    Obsidian toggle

--- nvim/.config/nvim/lua/custom/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/custom/plugins/obsidian.lua
@@ -25,6 +25,35 @@ return {
       },
     },
 
+    daily_notes = {
+      folder = 'Daily Notes',
+      template = 'Templates/Daily Template',
+    },
+
+    mappings = {
+      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+      ['gf'] = {
+        action = function()
+          return require('obsidian').util.gf_passthrough()
+        end,
+        opts = { noremap = false, expr = true, buffer = true },
+      },
+      -- Toggle check-boxes.
+      ['<leader>ch'] = {
+        action = function()
+          return require('obsidian').util.toggle_checkbox()
+        end,
+        opts = { buffer = true },
+      },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ['<cr>'] = {
+        action = function()
+          return require('obsidian').util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
+    },
+
     -- see below for full list of options 👇
   },
 }
